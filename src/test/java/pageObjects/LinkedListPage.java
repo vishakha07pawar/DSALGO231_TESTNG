94f29ff75057d5a91c318ccd5a16c4ff186354e9
Prakash Pawar <vishakhabir7@gmail.com>
2026-01-21 18:11:59 -0500

    commit pages

--- src/test/java/pageObjects/LinkedListPage.java
+++ src/test/java/pageObjects/LinkedListPage.java
@@ -1,14 +1,11 @@
 package pageObjects;
 
 
-import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LinkedListPage {
 	private WebDriver driver;
