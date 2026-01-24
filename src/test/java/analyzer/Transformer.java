8155e22a82acc39b3e631ac16e67869ef670cb9b
Prakash Pawar <vishakhabir7@gmail.com>
2026-01-24 09:12:25 -0500

    committing two files

--- src/test/java/analyzer/Transformer.java
+++ src/test/java/analyzer/Transformer.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Method;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
-public class MyTransformerAnalyzer implements IAnnotationTransformer{
+public class Transformer implements IAnnotationTransformer{
 	public void transform(ITestAnnotation annotation,Class testClass,Constructor testConstructor,Method testMethod) {
 		annotation.setRetryAnalyzer(RetryAnalyzer.class);
 	}
