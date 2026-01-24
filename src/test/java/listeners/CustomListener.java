472fe562b2c761cc28fed49ad7de77999dd6fd2d
showri <showrijb@gmail.com>
2026-01-23 23:03:28 -0500

    Added allure report

--- src/test/java/listeners/CustomListener.java
+++ src/test/java/listeners/CustomListener.java
@@ -1,5 +1,6 @@
 package listeners;
 
+import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
@@ -7,10 +8,13 @@ import org.testng.ITestResult;
 import com.aventstack.chaintest.plugins.ChainTestListener;
 import factory.DriverManager;
 
+import java.io.ByteArrayInputStream;
+
 public class CustomListener implements ITestListener {
     public void onTestFailure(ITestResult result) {
         TakesScreenshot takesScreenshot = (TakesScreenshot) DriverManager.getDriver();
         byte[] screenShot = takesScreenshot.getScreenshotAs(OutputType.BYTES);
         ChainTestListener.embed(screenShot, "image/png");
+        Allure.addAttachment(result.getName(), new ByteArrayInputStream(screenShot));
     }
 }
