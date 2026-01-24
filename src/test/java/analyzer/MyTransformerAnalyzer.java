7998896a371d7bc6ade7fcedfc74b1960a901fac
Prakash Pawar <vishakhabir7@gmail.com>
2026-01-23 20:29:21 -0500

    commit Mytransformanalyzer

--- src/test/java/analyzer/MyTransformerAnalyzer.java
+++ src/test/java/analyzer/MyTransformerAnalyzer.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Method;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
-public class MyTransformer implements IAnnotationTransformer{
+public class MyTransformerAnalyzer implements IAnnotationTransformer{
 	public void transform(ITestAnnotation annotation,Class testClass,Constructor testConstructor,Method testMethod) {
 		annotation.setRetryAnalyzer(RetryAnalyzer.class);
 	}
